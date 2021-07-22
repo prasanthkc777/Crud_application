4c0c17c71bcd24f9c5a66907231778838786e874
prasanthkc777 <83226287+prasanthkc777@users.noreply.github.com>
2021-07-22 21:16:25 +0530

    Update new.py

--- crud/new.py
+++ crud/new.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.secret_key = "flash message"
 app.config['MYSQL_HOST']='localhost'
 app.config['MYSQL_USER']='root'
-app.config['MYSQL_PASSWORD']='Gauranitai27#'
+app.config['MYSQL_PASSWORD']='your_password'
 app.config['MYSQL_DB']='CrudApplication'
 
 mysql=MySQL(app)
